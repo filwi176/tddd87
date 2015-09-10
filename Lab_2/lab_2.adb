4793467c19f6be44aa1153065901eacd80285644
Fredrik Orädd <freor897@parlomba4.ida.liu.se>
2015-09-10 12:13:33 +0200

    lite menyer

--- Lab_2/lab_2.adb
+++ Lab_2/lab_2.adb
@@ -5,13 +5,14 @@ with Ada.Numerics; use Ada.Numerics;
 
 
 procedure Lab_2 is
-
-   -- Radian to degree
+   
+   -- Radian to degree ------------------------------------------------------
    function To_Degree(R: in Float) return Float is
       Degree : Float;
    begin
       Degree := 180.0 * R / Pi;
       
+      -- Controlling degree value
       loop 
 	 if Degree>360.0 then
 	    Degree := Degree - 360.0;
@@ -27,15 +28,46 @@ procedure Lab_2 is
       
       return Degree;
    end To_Degree;
+   -------------------------------------------------------------------------
    
+   -- Radian to degree menu ------------------------------------------------
+   procedure To_Degree_Menu is
+      Option : Integer;
+      Rad : Float;
+      Degree : Float;
+      
+   begin
+      loop
+	 Put ("Return, press 1");
+	 New_Line;
+	 Put ("Calculate, press 2");
+	 New_Line;
+	 Get (Option);
+	 if Option=1 then
+	    exit;
+	 elsif Option=2 then
+	    Put ("Enter radian value: ");
+	    Get (Rad);
+	    Degree := To_Degree(Rad);
+	    Put (Rad, Aft=>2, Exp=>0);
+	    Put (" radians equals ");
+	    Put (Degree, Aft=>2, Exp=>0);
+	    Put (" degrees.");
+	    New_Line;
+	    exit;
+	 end if;
+      end loop;
+   end To_Degree_Menu; 
+   ---------------------------------------------------------------------------
    
-   --Degree to radian
+   --Degree to radian---------------------------------------------------------
    function To_Radian(D: in Float) return Float is
       Radian : Float;
       
    begin
       Radian := Pi * D / 180.0;
       
+      -- Controlling radian value
       loop
 	 if Radian>2.0*Pi then
 	    Radian := Radian - 2.0*Pi;
@@ -51,24 +83,102 @@ procedure Lab_2 is
       
       return Radian;
    end To_Radian;
+   --------------------------------------------------------------------------
    
+   -- Degree to Radian menu--------------------------------------------------
+   procedure To_Radian_Menu is
+      Option : Integer;
+      Deg : Float;
+      Radian : Float;
+      
+   begin
+      loop
+	 Put ("Return, press 1");
+	 New_Line;
+	 Put ("Calculate, press 2");
+	 New_Line;
+	 Get (Option);
+	 if Option=1 then
+	    exit;
+	 elsif Option=2 then
+	    Put ("Enter degree value: ");
+	    Get (Deg);
+	    Radian := To_Radian(Deg);
+	    Put (Deg, Aft=>2, Exp=>0);
+	    Put (" degrees equals ");
+	    Put (Radian, Aft=>2, Exp=>0);
+	    Put (" radians. ");
+	    New_Line;
+	    exit;
+	 end if;
+      end loop;
+   end To_Radian_Menu; 
+   --------------------------------------------------------------------------
    
-   Degree : Float;
-   Rad : Float;
-   Deg : Float;
-   Radian : Float;
+   -- Vector length menu-----------------------------------------------------
+   procedure Vector_Length_Menu is
+      
+      Option : Integer;
+      
+   begin
+      loop 
+	 Put ("Return, press 1");
+	 Put ("Calculate, press 2");
+	 if Option=1 then
+	    exit;
+	 elsif Option=2 then
+	    -- Vector_Length_Function
+	 end if;
+      end loop;
+      
+      
+      
+end Vector_Length_Menu;
+
+   
+   Option : Integer;
    
 begin
    
-   Get (Rad);
-   Degree := To_Degree(Rad);
-   Put (Degree, Aft=>2, Exp=>0);
+   -- Huvudmeny
    
-   New_Line;
+   loop 
+      Put ("Radians to degrees, press 1");
+      New_Line;
+      Put ("Degrees to radians, press 2");
+      New_Line;
+      Put ("Calculating vector length, press 3");
+      New_Line;
+      Put ("Float to Integer, press 4");
+      New_Line;
+      Put ("To create vector data, press 5");
+      New_Line;
+      Get (Option);
+      if Option=1 then
+	 -- To_Degree_menu
+	 To_Degree_Menu;
+
+	 
+      elsif Option=2 then
+	 -- To_Radian_menu
+	 To_Radian_Menu;
+	 
+	 --  elsif Option=V then
+	 --  	 -- Underprogrammeny Vector_Length
+	 
+	 --  elsif Option=I then
+	 --  	 -- Underprogrammeny To_Integer
+	 
+	 --  elsif Option=C then
+	 --  	 -- Underprogrammeny Create_Vector_Data
+	 
+	 
+      end if;
+      
+      
+   end loop;
    
-   Get (Deg);
-   Radian := To_Radian(Deg);
-   Put (Radian, Aft=>2, Exp=>0);
+   
    
 end Lab_2;
 
