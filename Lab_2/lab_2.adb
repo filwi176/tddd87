870e01168e9bd0ed11a8790d6dd333af7a895fd0
Elina Jansson <elija476@parlomba4.ida.liu.se>
2015-09-10 12:11:01 +0200

    Lägg till funktion Vector_Length i HP

--- Lab_2/lab_2.adb
+++ Lab_2/lab_2.adb
@@ -2,7 +2,7 @@ with ada.text_IO; use ada.Text_IO;
 with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 with Ada.Float_Text_IO; use Ada.Float_Text_IO;
 with Ada.Numerics; use Ada.Numerics;
-
+with Ada.Numerics.Generic_Elementary_Functions; 
 
 procedure Lab_2 is
 
@@ -52,6 +52,35 @@ procedure Lab_2 is
       return Radian;
    end To_Radian;
    
+   --Function Vector length
+   
+   package My_Elementary_Functions is
+      new Ada.Numerics.Generic_Elementary_Functions(Float);
+   use My_Elementary_Functions;
+   
+   function Vector_Length(X,Y,Z: in Float) return Float is
+      
+      Length : Float;
+      Hypotenuse : Float;
+      
+   begin 
+      
+
+      Hypotenuse:= Sqrt(X**2 + Y**2);
+      Length:= Sqrt(Hypotenuse**2 + Z**2);
+      
+      return Length;
+      
+      
+   end Vector_Length;
+   
+   
+   
+   ------------------------   
+   X : Float;
+   Y : Float;
+   Z : Float;
+   Length : Float;
    
    Degree : Float;
    Rad : Float;
@@ -59,8 +88,28 @@ procedure Lab_2 is
    Radian : Float;
    
 begin
-   
-   Get (Rad);
+   Put("Testing function Vector_Length. Write X-coordinate: ");
+	 Get(X);
+       Skip_Line;
+       Put("Write Y-coordinate: ");
+       Get(Y);
+       Skip_Line;
+       Put("Write Z-coordinate: ");
+       Get(Z);
+       Skip_Line;
+       
+       Length:= Vector_Length(X,Y,Z);
+       
+       Put("The length of the vector is: ");
+       Put(Length, Aft=>2, Exp=>0);
+       
+       New_Line(2);
+	 
+	 
+	 ------------------
+	 
+	 Put("Testing To_Degree and To_Rad: "); 
+       Get (Rad);
    Degree := To_Degree(Rad);
    Put (Degree, Aft=>2, Exp=>0);
    
