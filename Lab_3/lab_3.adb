b2dcfdc827adcfba087aa35804a7fb51f9760f10
Elina Jansson <elija476@li15-24.ida.liu.se>
2015-09-25 17:05:13 +0200

    Klar med Del B

--- Lab_3/lab_3.adb
+++ Lab_3/lab_3.adb
@@ -15,12 +15,12 @@ procedure Lab_3 is
    
    type Generate_Pair is
      array(1..2) of Integer;
-  Random_Generate_Pair : Generate_Pair;
+
    
    type Generate_Pair_Array is
      array(1..20) of Generate_Pair;
    
-Random_Generate_Pair_Array : Generate_Pair_Array;
+   Random_Generate_Pair_Array : Generate_Pair_Array;
    
    
    
@@ -38,15 +38,15 @@ Random_Generate_Pair_Array : Generate_Pair_Array;
       Arr : Generate_Type;
    begin
       OneTen_Integer_Random.Reset(G);
-     
+      
       Arr:= (others => OneTen_Integer_Random.Random(G));
       
       return Arr;
-     
+      
    end Generate;
    
    
-     
+   
    -------------------------------------------------
    function Generate return Generate_Pair_Array is
       
@@ -66,44 +66,20 @@ Random_Generate_Pair_Array : Generate_Pair_Array;
       
    end Generate;
    
-      -------------------------------------------------
-   -- SKRÄP?
-   --  function Generate return Generate_Pair_Array is
-      
-   --     subtype OneTen_Integer is Integer range 1..10;
-      
-   --     package OneTen_Integer_Random is
-   --  	 new Ada.Numerics.Discrete_Random(Result_Subtype => OneTen_Integer);
-      
-   --     G : OneTen_Integer_Random.Generator;
-   --     Pair : Generate_Pair;
-   --     Arr : Generate_Pair_Array;
-   --  begin
-   --    OneTen_Integer_Random.Reset(G);
-      
-   --     Pair := (others => OneTen_Integer_Random.Random(G));
-      
-      
-   --     Arr := (others => (others => Pair));
-      
-   --     return Arr;
-      
-   --  end Generate;
-   
-   
+   -------------------------------------------------
    
    --------------------------------------------   
    
- 
+   
    procedure Put(Arr: in Generate_Type) is
       
    begin  
       
       for I in Arr'Range loop
 	 
-	 Put(Arr(I));
+	 Put(Arr(I), Width => 6);
 	 New_Line;
-	
+	 
       end loop;
       
    end Put;
@@ -116,30 +92,41 @@ Random_Generate_Pair_Array : Generate_Pair_Array;
    begin  
       
       for I in Arr'Range loop
-	 Put(Arr(I)(1));
-	 Put(Arr(I)(2));
+	 Put(Arr(I)(1), Width => 4);
+	 Put(Arr(I)(2), Width => 4);
    	 New_Line;
       end loop;
       
    end Put;
-   
-   
-   -----------------------------------------------
+   ----------------------------------------
+   procedure Swap(A, B : in out Integer) is
+      Swapper: Integer;
+   begin
+      Swapper := A;
+      A := B;
+      B := Swapper;      
+   end Swap;
+   ------------------------------------------
+   procedure Swap(A, B : in out Generate_Pair) is
+      Swapper: Generate_Pair;
+   begin
+      Swapper:= (others => 0);
+      Swapper := A;
+      A := B;
+      B := Swapper;      
+   end Swap;
+   ------------------
+   --DEL A-----------------------------
    procedure Sort(Arr : in out Generate_Type) is 
       
-      
-      Swapper : Generate_Type;
-      
    begin 
-      Swapper := (others => 0);
+
       
       for J in (Arr'First + 1)..Arr'Last loop
 	 
 	 for I in reverse J.. Arr'Last loop
 	    if Arr(I) < Arr(I-1) then
-	       Swapper(I) := Arr(I) ;
-	       Arr(I) := Arr(I-1);
-	       Arr(I-1) := Swapper(I);
+	       Swap(Arr(I),Arr(I-1)); 
 	    end if; 
 	 end loop;
 	 
@@ -147,22 +134,46 @@ Random_Generate_Pair_Array : Generate_Pair_Array;
       
       
    end Sort;
+   
+   
+
+   
    -----------------------------------------------
    procedure Sort(Arr : in out Generate_Pair_Array) is 
       
       
-      Swapper : Generate_Pair_Array;
+      
+      function "<"(Pairone, Pairtwo : in Generate_Pair) return Boolean is
+	 
+	 
+      begin
+
+	 if Pairtwo(2) > Pairone(2) then
+	    return True;
+	    
+	 elsif  Pairone(2) = Pairtwo(2) and Pairtwo(1) > Pairone(1) then
+	    return True;
+	    
+	 else
+	    return False;  
+	    
+	    
+	 end if;
+	 
+	 
+	 
+      end "<";
+      
+      
       
    begin 
-      Swapper := (others => (others => 0));
+
       
       for J in (Arr'First + 1)..Arr'Last loop
 	 
 	 for I in reverse J.. Arr'Last loop
 	    if Arr(I) < Arr(I-1) then
-	       Swapper(I) := Arr(I) ;
-	       Arr(I) := Arr(I-1);
-	       Arr(I-1) := Swapper(I);
+	       Swap(Arr(I), Arr(I-1)); 
 	    end if; 
 	 end loop;
 	 
@@ -171,60 +182,85 @@ Random_Generate_Pair_Array : Generate_Pair_Array;
       
    end Sort;
 
-   -------
+   -----------------------------------------------
+   --  procedure Sort(Arr : in out Integer) is 
+   
+   --  begin 
+
+   
+   --     for J in (Arr'First + 1)..Arr'Last loop
+   
+   --  	  for I in reverse J.. Arr'Last loop
+   --  	     if Arr(I) < Arr(I-1) then
+   --  		Swap(Arr(I), Arr(I-1)); 
+   --  	     end if; 
+   --  	  end loop;
+   
+   --     end loop;     
+   
+   
+   --  end Sort;
+
+   
    -------------------------------------------------   
 begin
    
-   --  -- DEL A
-   --  -- Generera 20 slumptal
-   --  Random := Generate;
+   -- DEL A
    
-   --  -- Skriv ut osorterat
-   --  Put("Osorterade tal: ");
-   --  Put(Random);
+   Put("DEL A");
+   New_Line;
+   -- Generera 20 slumptal
+   Random := Generate;
    
-   --  New_Line;
+   -- Skriv ut osorterat
+   Put("Osorterade tal: ");
+   New_Line;
+   Put(Random);
    
-   --  -- Sortera fältet Random
-   --  Put("Sorterade tal: ");
-   --  New_Line;
+   New_Line;
    
-   --  Sort(Random);
+   -- Sortera fältet Random
+   Put("Sorterade tal: ");
+   New_Line;
    
-   --  -- Skriv ut sorterat
-   --  Put(Random);
+   Sort(Random);
    
-   --  New_Line(2);
-   ------------------------------------------------------
+   -- Skriv ut sorterat
+   Put(Random);
+   
+   New_Line(2);
+   
+   --------------------------------
    
    
    -- DEL B
-   
+   Put("DEL B");
+   New_Line;
    -- Generera slumptal till talpar
    Random_Generate_Pair_Array := Generate;
    
 
    -- Skriv ut osorterade talpar
+   Put("Osorterade tal: ");
+   New_Line;
    Put(Random_Generate_Pair_Array);
    
    New_Line;
-      
-   -- Sortera talar med avseende på andra talet och sedan på det första talet
+   
+   -- Sortera talar med avseende på andra talet och sedan på det första tale
+   Put("Sorterade tal: ");
+   New_Line;
 
-   --  for I in 1..20 loop
-       Sort(Random_Generate_Pair_Array(I));
-      
-      
-      
-      
-      
-   --     end loop;
-         
-   --  -- Skriv ut sorterade talpar, ett talpar per rad
-   
-        Put(Random_Generate_Pair_Array);
+   Sort(Random_Generate_Pair_Array);
    
    
+
+   
+   -- Skriv ut sorterade talpar, ett talpar per rad
+   
+   Put(Random_Generate_Pair_Array);
+   
+   New_Line(2);
    
    ------------------------------------------------------
 
